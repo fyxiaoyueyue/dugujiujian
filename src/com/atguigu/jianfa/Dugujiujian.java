e8a6a2cca34112148045f56bc6ed5e17ce4669a9
fyxiaoyue <m15733100917@163.com>
2017-08-18 00:55:47 +0800

    修改

--- src/com/atguigu/jianfa/Dugujiujian.java
+++ src/com/atguigu/jianfa/Dugujiujian.java
@@ -3,5 +3,10 @@ package com.atguigu.jianfa;
 public class Dugujiujian {
 	public static void main(String[] args) {
 		System.out.println("hello Dugujiujian");
+		System.out.println("hello Dugujiujian");
+		System.out.println("hello Dugujiujian");
+		System.out.println("hello Dugujiujian");
+		System.out.println("hello Dugujiujian");
+		System.out.println("hello Dugujiujian");
 	}
 }
