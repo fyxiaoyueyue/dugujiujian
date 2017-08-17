af7093b76f273af4683ebd7c9e243a42c4763ac2
fyxiaoyueyue <499606319@qq.com>
2017-08-18 01:00:16 +0800

    修改

--- src/com/atguigu/jianfa/Dugujiujian.java
+++ src/com/atguigu/jianfa/Dugujiujian.java
@@ -2,6 +2,10 @@ package com.atguigu.jianfa;
 
 public class Dugujiujian {
 	public static void main(String[] args) {
-		System.out.println("hello Dugujiujian");
+		System.out.println("hello999 Dugujiujian");
+		System.out.println("hello999 Dugujiujian");
+		System.out.println("hello999 Dugujiujian");
+		System.out.println("hello999 Dugujiujian");
+		System.out.println("hello999 Dugujiujian");
 	}
 }
